f4cf18d9d1d60749c6bd84f5e9d1bd71b4143152
Mahesh-Umachagi <maheshmumachagi@gmail.com>
2020-02-21 17:09:41 +0530

    first commit

---
+++ app/butterfly/butterfly.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-butterfly',
+  templateUrl: './butterfly.component.html',
+  styleUrls: ['./butterfly.component.css']
+})
+export class ButterflyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
